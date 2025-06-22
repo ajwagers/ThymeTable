f0a6a5596939acf055179a35b3d2e70ca24429dd
Andy J. Wagers <44385569+ajwagers@users.noreply.github.com>
2025-06-21 22:34:39 -0400

    Updated SubscriptionPage.tsx

--- src/pages/SubscriptionPage.tsx
+++ src/pages/SubscriptionPage.tsx
@@ -103,7 +103,7 @@ function SubscriptionPage() {
           ? 'border-terra-500 bg-gradient-to-br from-lemon-50 to-terra-50 shadow-lg scale-105' 
           : current
             ? 'border-green-500 bg-green-50'
-            : 'border-lemon-200 bg-lemon-70 hover:border-lemon-300'
+            : 'border-lemon bg-yellow-70 hover:border-yellow-300'
       }`}
       whileHover={{ scale: popular ? 1.05 : 1.02 }}
       initial={{ opacity: 0, y: 20 }}
