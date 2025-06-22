d9088487b517b43f9caa0b222d92798453693c13
Andy J. Wagers <44385569+ajwagers@users.noreply.github.com>
2025-06-21 22:27:17 -0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -163,7 +163,7 @@ const Header: React.FC = () => {
                 {currentTier !== 'premium' && (
                   <button
                     onClick={() => navigate('/subscription')}
-                    className="btn-primary bg-gradient-to-r from-yellow-500 to-orange-500 hover:from-yellow-600 hover:to-orange-600 text-white font-medium"
+                    className="btn-primary bg-gradient-to-r from-lemon-500 to-terra-500 hover:from-lemon-600 hover:to-terra-600 text-white font-medium"
                   >
                     <Crown className="h-4 w-4 mr-1" />
                     <span>Upgrade</span>
