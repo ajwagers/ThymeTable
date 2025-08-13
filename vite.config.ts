d78df3766033d39d291144ba5355d8bd159b4073
Andy J. Wagers <44385569+ajwagers@users.noreply.github.com>
2025-08-13 16:37:19 -0400

    Fix vite.config.ts syntax error

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.md'],
   assetsInclude: ['**/*.md']
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-});
